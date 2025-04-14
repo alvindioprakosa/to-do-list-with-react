7185fc7565370519605e44085a56b6433693189b
alvindioprakosa <alvindioprakosa@gmail.com>
2025-04-14 11:21:00 +0700

    Update Header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="header">
+    <header className="header">
       <div data-cy="header-background" className="container">
         <h2 data-cy="header-title">TO DO LIST APP</h2>
       </div>
-    </div>
+    </header>
   );
 }
 
